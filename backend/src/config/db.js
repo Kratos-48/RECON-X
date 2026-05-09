a17753fb09a279f00c6f15cf84ae25ba0a7e318e
Mohd Shakeeb Chougle <chouglesakeeb48@gmail.com>
2026-05-10 00:58:37 +0530

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,9 +1,16 @@
-import Database from "better-sqlite3";
+import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
+import Database from "better-sqlite3";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+
+const uploadsPath = path.join(__dirname, "../../uploads");
+if (!fs.existsSync(uploadsPath)) {
+  fs.mkdirSync(uploadsPath, { recursive: true });
+}
 
 const dbPath = path.join(__dirname, "../../recon-x.db");
 
